32e80bb353ad1b513c5178b497c5265659043ffd
Susan Minott <susan_minott@yahoo.com>
2022-06-14 10:15:18 -0400

    commit tree b94fbcf0a9e7e8d2900ad8bf254efecc22c5e209

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,22 @@
 class Dog
+
+    ActiveRecord::Base.establish_connection(
+        adapter: "sqlite3",
+        database: "./db/test.sqlite3"
+      )
+
+# Class inherits from ActiveRecord::Base
+    class Dog < ActiveRecord::Base
+    end
+
+# Create
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
 end
